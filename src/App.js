6e27cea58ad09b55a5c804ec8f5f765303ebf38a
Joaomgsa <joaomgsa@gmail.com>
2022-07-01 22:49:45 -0300

    Atualizacao da Logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './assets/logo-removebg.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Seja Bem vindo a
         </p>
+        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Em Breve nosso novo Site no ar
         </a>
       </header>
     </div>
