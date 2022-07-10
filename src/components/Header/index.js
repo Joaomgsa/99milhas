8ed7cad8676953ff86642dcbd9860c8d4dd976cd
Joaomgsa <joaomgsa@gmail.com>
2022-07-10 19:02:54 -0300

    responsividade no nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,35 +21,37 @@ export default function Header() {
             <Link to="/">
                 <img src={logo} className="logo-img" alt="99milhas"/>
             </Link>
-                <nav>
-                    <ul>
-                        <li>
-                            <NavLink to={"/Voos"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
-                            <GiAirplaneDeparture size={32}/>
-                                Voos
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={"/Pacotes"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
-                            <GiBeachBag size={32}/>
-                                Pacotes
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={"/Promo"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
-                            <GiStarProminences size={32}/>
-                                Promos 2022
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={"/Resort"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
-                            <GiFamilyHouse size={32}/>
-                                Resorts
-                            </NavLink>
-                        </li>
-                    </ul>
-                    
-                </nav>  
+                <div className="center">
+                    <nav>
+                        <ul className="topnav">
+                            <li>
+                                <NavLink to={"/Voos"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
+                                <GiAirplaneDeparture size={32}/>
+                                    Voos
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={"/Pacotes"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
+                                <GiBeachBag size={32}/>
+                                    Pacotes
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={"/Promo"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
+                                <GiStarProminences size={32}/>
+                                    Promos 2022
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to={"/Resort"} className="navlink-a" style={({isActive}) => isActive ? activeStyle : undefined }>
+                                <GiFamilyHouse size={32}/>
+                                    Resorts
+                                </NavLink>
+                            </li>
+                        </ul>
+                        
+                    </nav>
+                </div>  
             
         </header>
         
