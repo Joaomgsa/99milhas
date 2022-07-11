68b043d2c0940afa087638061f93dd2ceb3eb887
Joaomgsa <joaomgsa@gmail.com>
2022-07-11 08:50:28 -0300

    adicionando styled components

---
+++ src/pages/Voos/styles.js
@@ -0,0 +1,10 @@
+
+import styled from "styled-components"
+
+export const = ImgContainer = styled.div
+    position: relative;
+    height: 21rem;
+    background-size: ConvolverNode;
+    background-position: bottom;
+    background-attachment: fixed;
+`
